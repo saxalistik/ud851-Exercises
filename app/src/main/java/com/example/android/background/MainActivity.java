2dcf540d3f0c9e5330f0aa91ce30abc8ee9905f7
Michael Lustig <halfjew22@gmail.com>
2016-10-31 16:13:59 -0400

    T10.01-Solution-IntentServices

--- app/src/main/java/com/example/android/background/MainActivity.java
+++ app/src/main/java/com/example/android/background/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.background;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -23,6 +24,8 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.android.background.sync.ReminderTasks;
+import com.example.android.background.sync.WaterReminderIntentService;
 import com.example.android.background.utilities.PreferenceUtilities;
 
 public class MainActivity extends AppCompatActivity implements
@@ -86,7 +89,10 @@ public class MainActivity extends AppCompatActivity implements
         mToast = Toast.makeText(this, R.string.water_chug, Toast.LENGTH_SHORT);
         mToast.show();
 
-//      TODO (15) Start WaterReminderIntentService to increment the water count when the button is pressed
+//      COMPLETED (15) Start WaterReminderIntentService to increment the water count when the button is pressed
+        Intent incrementWaterCountIntent = new Intent(this, WaterReminderIntentService.class);
+        incrementWaterCountIntent.setAction(ReminderTasks.ACTION_INCREMENT_WATER_COUNT);
+        startService(incrementWaterCountIntent);
     }
 
     @Override
